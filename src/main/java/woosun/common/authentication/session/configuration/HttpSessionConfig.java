e82a619e35c267a409cbb2bf5b91c707df2e95d5
twosunny <singwoosun@gmail.com>
2017-03-29 16:20:55 +0900

    redis session

--- src/main/java/woosun/common/authentication/session/configuration/HttpSessionConfig.java
+++ src/main/java/woosun/common/authentication/session/configuration/HttpSessionConfig.java
@@ -1,5 +1,9 @@
 package woosun.common.authentication.session.configuration;
 
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -8,19 +12,32 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.util.StringUtils;
 
+import redis.embedded.RedisServer;
+
 @EnableRedisHttpSession
 public class HttpSessionConfig {
 	
 	@Autowired
 	private SessionAuthenticationProperties authenticationProperties;
-
+	
+	@PostConstruct
+	private void init(){
+		if(authenticationProperties.isUseEmbeddedRedis()){
+			RedisServer redisServer;
+			try {
+				redisServer = new RedisServer(6379);
+				redisServer.start();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
 	@Bean
 	public LettuceConnectionFactory connectionFactory() {
 		
 		LettuceConnectionFactory lettuceConnectionFactory = null;
-		
-		String host = authenticationProperties.getRedisHost();
-		if(StringUtils.isEmpty(host))return lettuceConnectionFactory;
 		
 		GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
 		poolConfig.setMaxTotal(authenticationProperties.getRedisPoolMaxTotal());
@@ -29,7 +46,8 @@ public class HttpSessionConfig {
 		poolConfig.setMaxWaitMillis(authenticationProperties.getRedisPoolMaxWaitMillis());
 		
 		
-		DefaultLettucePool lettucePool = new DefaultLettucePool(host, 
+		DefaultLettucePool lettucePool = new DefaultLettucePool(
+				authenticationProperties.getRedisHost(), 
 				authenticationProperties.getRedisPort(),
 				poolConfig);
 		lettucePool.afterPropertiesSet();
